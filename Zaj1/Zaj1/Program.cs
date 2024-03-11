33dcf02ad01a80f25657904617a8c4ee63f33469
s27003 <s27003@pjwstk.edu.pl>
2024-03-11 13:08:15 +0100

    Version 1.0

---
+++ Zaj1/Zaj1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zaj1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("testing 111");
+        }
+    }
+}
