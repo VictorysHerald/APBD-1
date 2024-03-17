346e2a7be5b5e9d3b2fa860bad33facae518ef08
s27003 <s27003@pjwstk.edu.pl>
2024-03-17 21:11:44 +0100

    added CalculateMax feature

--- Zaj1/Zaj1/Program.cs
+++ Zaj1/Zaj1/Program.cs
@@ -9,5 +9,21 @@ namespace Zaj1
             Console.WriteLine("Hello World");
             //komentarz kolego
         }
+
+        public static int CalculateMax(int[] tab)
+        {
+            int maxVal = tab[0];
+            for (int i = 1; i < tab.Length; i++)
+            {
+                if (tab[i] > maxVal)
+                {
+                    maxVal = tab[i];
+                }
+            }
+            return maxVal;
+        }
+        
+        
+        
     }
 }
